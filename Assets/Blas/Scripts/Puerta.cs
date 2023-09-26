e9ebe55b493c754e10d6d18986145f90862e5af7
santiERP <santino.chemez@davinci.edu.ar>
2023-09-26 12:53:41 -0300

    Blocking nivel

--- Assets/Blas/Scripts/Puerta.cs
+++ Assets/Blas/Scripts/Puerta.cs
@@ -28,6 +28,7 @@ public class Puerta : MonoBehaviour
     {
         desiredPosition = WhereDoIMoveTo + normalPosition;
     }
+
     void WhatToDont()
     {
         desiredPosition = normalPosition;
