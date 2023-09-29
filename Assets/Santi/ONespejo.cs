680a525be849e5b01513f2fb367e1db0ae87f59d
santiERP <santino.chemez@davinci.edu.ar>
2023-09-29 17:23:02 -0300

    Niveles unidos en una escena

---
+++ Assets/Santi/ONespejo.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ONespejo : MonoBehaviour
+{
+    [SerializeField] EspejoAntigravedad espejo;
+    [SerializeField] GameObject cubo;
+    // Start is called before the first frame update
+    private void OnTriggerEnter(Collider other)
+    {
+        espejo.gameObject.SetActive(false);
+        cubo.SetActive(true);
+        Destroy(gameObject);
+    }
+}
