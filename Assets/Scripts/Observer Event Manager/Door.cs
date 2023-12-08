e879292b8c82fccfcdf0e16f13db2dcbb0e13051
santiERP <santino.chemez@davinci.edu.ar>
2023-12-08 20:41:33 -0300

    avances

--- Assets/Scripts/Observer Event Manager/Door.cs
+++ Assets/Scripts/Observer Event Manager/Door.cs
@@ -6,9 +6,10 @@ public class Door : MonoBehaviour
 {
     [SerializeField] Interruptor _activationButton;
     [SerializeField] Vector3 _whereDoIMoveTo;
+    [SerializeField] float _doorVelocity = 0.05f;
     Vector3 _desiredPosition;
     Vector3 _normalPosition;
-
+    Rigidbody _rig;
     [SerializeField] AudioSource _audioSource;
 
     // Start is called before the first frame update
@@ -18,12 +19,14 @@ public class Door : MonoBehaviour
         _desiredPosition = _normalPosition;
         _activationButton.ButtonAction += WhatToDo;
         _activationButton.ButtonAntiAction += WhatToDont;
+        _rig = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    private void Update()
+    private void FixedUpdate()
     {
-        transform.position = Vector3.Lerp(transform.position, _desiredPosition, 0.05f);
+        _rig.MovePosition(Vector3.Lerp(transform.position, _desiredPosition, _doorVelocity));
+
     }
 
     void WhatToDo()
